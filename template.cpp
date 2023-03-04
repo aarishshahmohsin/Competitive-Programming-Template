3745beb481968f0f7ba24ae9475ac93ef09a24f6
aarishshahmohsin <49566965+aarishshahmohsin@users.noreply.github.com>
2023-03-04 12:19:46 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,140 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) {
-    if (vec.empty()) {
-        out << "[]";
-        return out;
-    }
-    out << '[';
-    for (int i = 0; i < vec.size() - 1; i++) {
-        out << vec[i] << ", ";
-    }
-    return out << vec.back() << ']';
-}
-
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& pair) {
-    return out << '(' << pair.first << ", " << pair.second << ')';
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::deque<T>& deq) {
-    if (deq.empty()) {
-        out << "[]";
-        return out;
-    }
-    out << '[';
-    for (int i = 0; i < deq.size() - 1; i++) {
-        out << deq[i] << ", ";
-    }
-    return out << deq.back() << ']';
-}
-
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::unordered_map<T1, T2>& map) {
-    out << '{';
-    for (auto it = map.begin(); it != map.end(); it++) {
-        std::pair<T1, T2> element = *it;
-        out << element.first << ": " << element.second;
-        if (std::next(it) != map.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::map<T1, T2>& map) {
-    out << '{';
-    for (auto it = map.begin(); it != map.end(); it++) {
-        std::pair<T1, T2> element = *it;
-        out << element.first << ": " << element.second;
-        if (std::next(it) != map.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::unordered_set<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::multiset<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::unordered_multiset<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::set<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-// Source: https://stackoverflow.com/a/31116392/12128483
-template<typename Type, unsigned N, unsigned Last>
-struct TuplePrinter {
-    static void print(std::ostream& out, const Type& value) {
-        out << std::get<N>(value) << ", ";
-        TuplePrinter<Type, N + 1, Last>::print(out, value);
-    }
-};
-
-template<typename Type, unsigned N>
-struct TuplePrinter<Type, N, N> {
-    static void print(std::ostream& out, const Type& value) {
-        out << std::get<N>(value);
-    }
-};
-
-template<typename... Types>
-std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& value) {
-    out << '(';
-    TuplePrinter<std::tuple<Types...>, 0, sizeof...(Types) - 1>::print(out, value);
-    return out << ')';
-}
-
 #ifdef DEBUG
-     #define debug(args...)            {dbg,args; cerr<<endl;}
+    #include </home/aarish/.config/debug.h>
+    #define debug(args...)            {dbg,args; cerr<<endl;}
 #else
     #define debug(args...)              // Just strip off all debug tokens
 #endif
@@ -159,7 +28,6 @@ struct debugger
 } dbg;
 
 void run_case() {
-    
 }
 
 int main() {
@@ -172,6 +40,7 @@ int main() {
     while (tt--) {
         run_case();
     }
+
 
     return 0;
 }
